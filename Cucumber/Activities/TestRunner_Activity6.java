f145fa26347e4a2e6cef82fecaec304ba83a6ceb
rashmipsmurthy <rashmi.ps@in.ibm.com>
2024-06-29 21:55:18 +0530

    Add files via upload

---
+++ Cucumber/Activities/TestRunner_Activity6.java
@@ -0,0 +1,19 @@
+package testrunner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/Features",
+        glue = {"stepDefinitions"},
+        tags = "@SmokeTest",
+        plugin = {"html: test-reports"},
+        monochrome = true
+
+)
+public class TestRunner {
+
+    //Empty
+}
